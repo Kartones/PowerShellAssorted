9ea2980868d671a378bc41daff1a2a00e671409a
Kartones <admin@kartones.net>
2012-12-01 12:42:07 +0100

    Readme + thumbs generation script

---
+++ README.md
@@ -0,0 +1,6 @@
+PowerShell Assorted Scripts
+=================
+
+Miscellaneous PowerShell scripts I've built to automate tasks at home and with my personal projects
+
+* `thumbs.ps1` : This script grabs all JPG files from current folder, generates a thumbnail in a subfolder, and stores a thumbs.list file containing all original file name and thumbnail filename.
---
+++ thumbs.ps1
@@ -0,0 +1,37 @@
+[Void][System.Reflection.Assembly]::LoadFile( "C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.Drawing.dll");
+
+$thumbsSubfolder = "thumbs\";
+$thumbsPrefix = "t_";
+$listFileName = "thumbs.list";
+$listFileSeparatorToken = "||";
+
+$images = get-childitem -Filter *.jpg;
+foreach ($image in $images)
+{ 
+	if (!$directory)
+	{
+		$directory = [System.IO.Path]::GetDirectoryName($image.FullName) + "\";
+		if (!(Test-Path ($directory + $thumbsSubfolder)))
+		{
+			New-Item ($directory + $thumbsSubfolder) -ItemType directory;
+		}
+		# Overrides if existing, no need to manually delete
+        $listFile = [System.IO.StreamWriter] ($directory + $listFileName);
+	}
+
+	$fullImage = [System.Drawing.Image]::FromFile($image.FullName); 
+	$thumb = $fullImage.GetThumbnailImage(150, 150, $null, [intptr]::Zero); 
+	$destination = $directory + $thumbsSubfolder + $thumbsPrefix + $image.Name;
+	Write-Host "THUMB:" , $destination;
+	$thumb.Save($destination);
+
+	$listFile.WriteLine($thumbsPrefix + $image.Name + $listFileSeparatorToken + $image.Name);
+
+	$fullImage.Dispose(); 
+	$thumb.Dispose();
+}
+
+if ($listFile)
+{
+    $listFile.close();
+}
